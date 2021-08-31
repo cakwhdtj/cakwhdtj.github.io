176b77df38986039607992aec581ce99ceafa3c5
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2021-08-31 15:55:13 +0900

    update
    - library

--- dev-react-pages/src/library/js/components/contentList.js
+++ dev-react-pages/src/library/js/components/contentList.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
 
-class ContentList extends Component {
+class Subheading extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         imageSlide: [
+    //             { id: 1, title: 'step', desc: 'imgae-slide' },
+    //             { id: 2, title: 'step', desc: 'imgae-slide' },
+    //             { id: 3, title: 'step', desc: 'imgae-slide' },
+    //             { id: 4, title: 'step', desc: 'imgae-slide' },
+    //             { id: 5, title: 'step', desc: 'imgae-slide' },
+    //             { id: 6, title: 'step', desc: 'imgae-slide' },
+    //             { id: 7, title: 'step', desc: 'imgae-slide' },
+    //             { id: 8, title: 'step', desc: 'imgae-slide' },
+    //             { id: 9, title: 'step', desc: 'imgae-slide' },
+    //         ],
+    //     }
+    // }
     render() {
+        var subheadLists = [];
         var lists = [];
         var menu = this.props.menu;
+        var content = this.props.content;
         var i = 0;
+        var j = 0;
+        while (j < content.length) {
+            lists.push(<p key={content[j].id}>{content[j].title}</p>);
+            j = j + 1;
+        }
+        console.log(lists)
         while (i < menu.length) {
-            lists.push(<li key={menu[i].id}><a href={"/contentlist/" + menu[i].id}>{menu[i].title}</a></li >);
+            subheadLists.push(
+                <div>
+                    <h4 key={menu[i].id} className={"divNum" + menu[i].id}>{menu[i].title}</h4>
+                    <ul>
+                        <li>{lists}</li>
+                    </ul>
+                </div>);
             i = i + 1;
         }
+        // console.log(content);
         return (
-            <ul className="ContentList">
-                {lists}
-            </ul>
+            < div className="UI" >
+                {subheadLists}
+            </div >
         );
     }
 }
 
-export default ContentList;
+export default Subheading;
--- dev-react-pages/src/library/js/components/main.js
+++ dev-react-pages/src/library/js/components/main.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import ContentList from './contentList';
+import Subheading from './contentList';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            contentlist: [
+            subheadings: [
                 { id: 1, title: '이미지 슬라이드', desc: 'imgae-slide' },
                 { id: 2, title: '배너 슬라이드', desc: 'banner-slide' },
                 { id: 3, title: '스크롤 UI', desc: 'scroll-UI' },
@@ -13,14 +13,25 @@ class Main extends Component {
                 { id: 5, title: '드래그 UI', desc: 'drag-UI' },
                 { id: 6, title: '내비게이션 UI', desc: 'nav-UI' },
                 { id: 7, title: '모바일 UI', desc: 'mobile_UI' },
-            ]
+            ],
+            imageSlide: [
+                { id: 1, title: 'step 1', desc: 'imgae-slide' },
+                { id: 2, title: 'step 2', desc: 'imgae-slide' },
+                { id: 3, title: 'step 3', desc: 'imgae-slide' },
+                { id: 4, title: 'step 4', desc: 'imgae-slide' },
+                { id: 5, title: 'step 5', desc: 'imgae-slide' },
+                { id: 6, title: 'step 6', desc: 'imgae-slide' },
+                { id: 7, title: 'step 7', desc: 'imgae-slide' },
+                { id: 8, title: 'step 8', desc: 'imgae-slide' },
+                { id: 9, title: 'step 9', desc: 'imgae-slide' },
+            ],
         }
     }
     render() {
         return (
             <main className="Main">
-                <ContentList menu={this.state.contentlist}></ContentList>
-            </main>
+                <Subheading menu={this.state.subheadings} content={this.state.imageSlide}></Subheading>
+            </main >
         )
     }
 }
