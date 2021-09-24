dc4e98d63a04cc663b30b0ec36e1ae45ec0c401d
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2021-09-24 15:24:49 +0900

    -fixin

--- dev-react-pages/src/library/js/components/main.js
+++ dev-react-pages/src/library/js/components/main.js
@@ -31,11 +31,24 @@ class Main extends Component {
             newImage.onload = () => {
                 imgSet.push(newImage)
                 this.setState({ imgSet })
-                // console.log(this.state)
+                console.log(this.state)
             }
         });
-        // console.log(imgSet)
+        console.log(imgSet)
     }
+    // componentDidMount() {
+    //     const imagesPreload = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10, Img11, Img12, Img13, Img14, Img15];
+    //     imagesPreload.forEach((image) => {
+    //         const newImage = new Image();
+    //         newImage.src = image;
+    //         window[image] = newImage;
+    //         newImage.onload = () => {
+    //             this.setState(newImage)
+    //         }
+    //         console.log(newImage);
+    //     });
+    //     console.log(this.state)
+    // }
     constructor(props) {
         super(props);
         this.state = {
