96cf9db1a6f9218b585590500b373bde2feee672
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2022-03-03 17:34:42 +0900

    - pr

--- assets/portfolio/practice/react-practice/src/App.js
+++ assets/portfolio/practice/react-practice/src/App.js
@@ -13,13 +13,22 @@ function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
   const [showing, setShowing] = useState(false);
-
-  
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const _onClick = () => setValue((prev) => prev +1);
   const _onChange = (e) => setKeyword(e.target.value);
-
   const _onClick2 = () => setShowing((prev)=>!prev);
-
+  const _onChange2 = (e) => setToDo(e.target.value);
+  const _onSubmit = (e) => {
+    e.preventDefault();
+    console.log('submitted', {toDo});
+    if (toDo === "") {
+      return;
+    } else {
+      setToDos(currentArray => [toDo, ...currentArray]);
+      setToDo("");
+    }
+  }
 
   useEffect(()=> {
     // if (keyword !== "" && keyword.length > 5) {
@@ -29,7 +38,19 @@ function App() {
   useEffect(()=> {
       console.log('counter changes');
   },[counter]);
+// ------------
+const [loading, setLoading] = useState(true);
+const [coins, setCoins] = useState([]);
+useEffect(()=>{
+  fetch("https://api.coinpaprika.com/v1/tickers")
+  .then((response)=>response.json())
+  .then((json) =>{
+    setCoins(json);
+    setLoading(false);
+  });
+},[]);
 
+// ------------
   return (
     <div>
       {showing ? <Hello /> : null}
@@ -37,11 +58,41 @@ function App() {
         value={keyword}
         onChange={_onChange} 
         type="text" 
-        placeholder="Search here... "/>
+        placeholder="Search here... "/> <br />
+      <h1>My Todos ({toDos.length})</h1>
+      <form onSubmit={_onSubmit}>
+        <input 
+        type="text"
+        placeholder="Write the thing"
+        onChange={_onChange2}
+        value={toDo}
+        />
+        <button>Add todo</button>
+      </form>
+      <hr />
+        <ul>
+          {toDos.map((item_toDO, index) => (
+          <li key={index}>{item_toDO} <button>❌</button></li>
+          ))}
+        </ul>
+      <hr />
       <h1>{counter}</h1>
       <button onClick={_onClick}>click me</button>
       <button onClick={_onClick2}>{showing ? "hide" : "show"}</button>
-
+      
+      <div>
+        <h1>The Coins</h1>
+        {loading ? <strong>Loading...</strong> : null}
+        {loading ? "":
+          <select>
+            {coins.map((coin)=> 
+            ((coin.quotes.USD.price).toFixed(3)>1)?
+            (<option key={coin.id}>{coin.name}({coin.symbol}) : ${(coin.quotes.USD.price).toFixed(3)} USD</option>)
+            :null)
+            }
+          </select>
+        }
+      </div>
     </div>
   );
 }
