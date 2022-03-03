b87edf980803e79b6aa849af4d0aeeea0236b4ee
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2022-03-03 13:18:54 +0900

    -react js practice

--- assets/portfolio/practice/react-js/vanilla.js
+++
@@ -1,9 +0,0 @@
-let counter = 0;
-const _button = document.getElementById("btn");
-const _span = document.querySelector("span");
-function handleClick() {
-    counter = counter + 1;
-    _span.innerText = `Total clicks: ${counter}`;
-}
-
-_button.addEventListener("click", handleClick);
