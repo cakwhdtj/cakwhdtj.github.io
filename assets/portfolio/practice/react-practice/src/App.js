0f2d0b39cf076c8226fe9901d2baab1d8749d8ea
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2022-03-03 14:35:35 +0900

    - pr

--- assets/portfolio/practice/react-practice/src/App.js
+++ assets/portfolio/practice/react-practice/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+
+function Hello() {
+  useEffect(()=>{
+    console.log('created');
+    return () => console.log("destroyed")
+  },[])
+  return <h1>Hellooooo</h1>
+}
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
+
+  
+  const _onClick = () => setValue((prev) => prev +1);
+  const _onChange = (e) => setKeyword(e.target.value);
+
+  const _onClick2 = () => setShowing((prev)=>!prev);
+
+
+  useEffect(()=> {
+    // if (keyword !== "" && keyword.length > 5) {
+      console.log('keyword changes');
+    // }
+  },[keyword]);
+  useEffect(()=> {
+      console.log('counter changes');
+  },[counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showing ? <Hello /> : null}
+      <input 
+        value={keyword}
+        onChange={_onChange} 
+        type="text" 
+        placeholder="Search here... "/>
+      <h1>{counter}</h1>
+      <button onClick={_onClick}>click me</button>
+      <button onClick={_onClick2}>{showing ? "hide" : "show"}</button>
+
     </div>
   );
 }
