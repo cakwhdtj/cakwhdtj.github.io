af9e803a39074868fa40aefd38ea9ffc02eac545
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2022-06-11 00:20:02 +0900

    - update
    react settup

---
+++ assets/portfolio/portfolio_CJ_AI/cj/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+const Header = () => {
+    return (
+      <header>
+        <h1>header</h1>
+      </header>
+    );
+  };
+  
+export default Header;
---
+++ assets/portfolio/portfolio_CJ_AI/cj/src/components/Main.js
@@ -0,0 +1,13 @@
+import Header from './Header'
+
+const Main = () => {
+    return (
+        <div>
+        <Header />
+            <h1>홈</h1>
+            <p>가장 먼저 보여지는 페이지입니다.</p>
+        </div>
+    );
+  };
+  
+  export default Main;
