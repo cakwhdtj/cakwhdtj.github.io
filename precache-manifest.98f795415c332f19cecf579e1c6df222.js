982462a790ec98a84fe3f4a601ce4480af11fe2d
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2022-06-21 21:32:01 +0900

    Updates

---
+++ precache-manifest.98f795415c332f19cecf579e1c6df222.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bd8df0bec338d83efdd",
+    "url": "/cj_ai_react/static/css/main.64004c66.chunk.css"
+  },
+  {
+    "revision": "8bd8df0bec338d83efdd",
+    "url": "/cj_ai_react/static/js/main.8bd8df0b.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/cj_ai_react/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "9c41f08bb158cd29279d",
+    "url": "/cj_ai_react/static/js/2.9c41f08b.chunk.js"
+  },
+  {
+    "revision": "2bfe61f0453f7d785b1d",
+    "url": "/cj_ai_react/static/js/runtime~main.2bfe61f0.js"
+  },
+  {
+    "revision": "fb4281a9eef740f32b8c8927c4cdad61",
+    "url": "/cj_ai_react/static/media/CJ+ONLYONE+Medium.fb4281a9.ttf"
+  },
+  {
+    "revision": "261b2b84eeea4ba92a59cfebaea8e7ba",
+    "url": "/cj_ai_react/static/media/CJ+ONLYONE+Bold.261b2b84.ttf"
+  },
+  {
+    "revision": "e7302464b47343f03d4ce1a9816e02b0",
+    "url": "/cj_ai_react/index.html"
+  }
+];
