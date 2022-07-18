edb71b081b002610e450195c736ded6a8e5faf1d
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2022-07-18 15:51:35 +0900

    - update
    header responsive

--- dev-cj-ai/src/components/Header.js
+++ dev-cj-ai/src/components/Header.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SVG from "./SvgSet";
 
 const Header = (props) => {
   let winWidth = props.dataSet.width;
   let scroll = props.dataSet.scroll;
+  const [isOn , toggleOn] = useState(false);
+  const handleClick = () => {
+    isOn ? toggleOn(false) : toggleOn (true);
+  }
+  if (winWidth > 1024 && isOn) {toggleOn(false)};
 
   // eslint-disable-next-line
   const headerBg = () => {
@@ -13,14 +18,14 @@ const Header = (props) => {
     // eslint-disable-next-line
     {(scroll > 10) ? result = 'black' : result = result}
     // eslint-disable-next-line
-    {(scroll > 1040) ? result = 'white' : result = result}
+    {(scroll > 1040) ? result = '#fff' : result = result}
     // eslint-disable-next-line
     return result
   } 
   const fontSizer = () => {
     let result = (winWidth/1512)*100;
     // eslint-disable-next-line
-    (result > 100) ? result = 100 : result = result;
+      (result > 100) ? result = 100 : result = result;
     return result;
   }
   const ulStyle = {
@@ -28,7 +33,7 @@ const Header = (props) => {
     color: `${props.borw}`,
   }
   const headerStyle = {
-    background : `${headerBg()}`,
+    background : `${(isOn && (winWidth < 1024)) ? "#fff" : headerBg()}`,
     boxShadow : `${(scroll > 10) ? 'rgb(0 0 0 / 12%) 1px 1px 1px 1px' : 'none'}`
   }
   const lists = props.menuList;
@@ -36,21 +41,31 @@ const Header = (props) => {
           lists.map((lists,index) => 
             <li key={index +"_"+lists}><Link to={`#section`+(index+2)}>{lists}</Link></li>);
   const btn = (on) => 
-          <button className={on} id="headerCloseBtn" onClick={()=>console.log('hi')}><span></span><i className="hide_clipPath">닫기</i><span></span></button>
-  
+          <div className="on">
+            <button className={`${isOn ? "on" : ""}`} id="headerCloseBtn" onClick={()=>handleClick()}>
+              <span></span>
+              <i className="hide_clipPath">닫기</i>
+              <span></span>
+            </button>
+            <ul className={`${isOn ? "on" : ""}`} style={ulStyle}>
+              {headerList}
+            </ul>
+          </div>
+  console.log(isOn)
       return (
       <header style={headerStyle}>
         <div id="headerContainer">
           <h1>
             <Link to="#">
-              <SVG type={'logo'} size={0} circle={[]} BorW={props.borw}/>
+              <SVG type={'logo'} size={0} circle={[]} BorW={isOn ? "#000" : props.borw}/>
             </Link>
           </h1>
           <nav id="gnb">
-            <ul style={ulStyle}>
+            {(winWidth > 1024) ? 
+              <ul style={ulStyle}>
               {headerList}
-            </ul>
-            {(winWidth < 1024) ? btn("on") : btn("")}
+              </ul> : null}
+            {(winWidth <= 1024) ? btn("on") : null}
           </nav>
         </div>
       </header>
