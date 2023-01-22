74b6241b50edd092a75eb05d0d67cbd733abb5d9
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2023-01-22 17:00:36 +0900

    -

--- assets/portfolio/portfolio_Stove/js/common_js.js
+++ assets/portfolio/portfolio_Stove/js/common_js.js
@@ -1,6 +1,22 @@
+const { indexOf } = require("lodash");
+
 $('a[href="#"]').on('click', function (e) {
   e.preventDefault();
 });
 
-console.log('hi')
+document.querySelector(".slide_container").addEventListener("click" , e => {
+  showSlide(e);
+})
+
+
+let showSlide = (e) => {
+  let selector = $(e.currentTarget);
+  console.log(indexOf(selector.find('li.on')))
+  // selector.find(".indicator li").removeClass('on');
+
+}
+
+
+
+
 //portfolio global
