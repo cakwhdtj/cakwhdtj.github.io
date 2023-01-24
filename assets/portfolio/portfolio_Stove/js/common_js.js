39f9cbe763416189981a4ae7f6b41252c6a6c622
cakwhdtj <cakwhdtj@gmail.com>
2023-01-24 22:10:40 +0900

    -

--- assets/portfolio/portfolio_Stove/js/common_js.js
+++ assets/portfolio/portfolio_Stove/js/common_js.js
@@ -2,15 +2,16 @@ $('a[href="#"]').on('click', function (e) {
   e.preventDefault();
 });
 
-$("body").on("click" , function (e) {
+$("#games").on("click" , function (e) {
   onAdder(e, '#games');
   console.log(e)
 });
 
 
 
-let onAdder = (e) => {
+let onAdder = (e , container) => {
   let selector = e.target;
+  let parents = $(container)
   $(selector).closest('li').siblings('.on').removeClass('on')
   $(selector).closest('li').toggleClass('on');
 }
