49d4ddb069585df875ec3515a372b520a12eb457
cakwhdtj <cakwhdtj@gmail.com>
2023-02-03 21:23:02 +0900

    -

--- assets/portfolio/portfolio_Stove/js/common_js.js
+++ assets/portfolio/portfolio_Stove/js/common_js.js
@@ -16,13 +16,23 @@ function onAdder(e , container) {
 function setSlides(selector) {
   let $selector = $(selector);
   let numSlide = $selector.find(".slides > li").length;
+  let n = 0;
+  let slideNow = 0;
+  let slidePrev = n - 1;
+  let slideNext = n + 1;
+
   $selector.find('.slides > li').each((i) => {
     $selector.find('.indicator').append('<li><a href="#"><span  class="hide_clipPath">' + (i + 1) + '번 슬라이드</span></a></li>\n');
   });
+  $selector.find(".control > button").on("click", function (e) {
+    console.log(n);
+    $(e.currentTarget).hasClass("left") ? showSlide(slidePrev ,  $selector.find('.indicator')) : showSlide(slideNext , $selector.find('.indicator'));
+  })
 }
-function showSlide(n , selector){
-  $selector.find('.indicator li').removeClass('on');
-  $selector.find('.indicator li:eq(' + (n - 1) + ')').addClass('on');
+function showSlide(n , selector) {
+  let $selector = $(selector);
+  // $selector.find('.indicator li').removeClass('on');
+  onAdder($selector.find('li:eq(' + (n - 1) + ')'));
 }
 
 
