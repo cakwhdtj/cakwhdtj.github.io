5522be44ddd9abb6b74718320e7c98dc92281206
cakwhdtj <cakwhdtj@gmail.com>
2023-02-03 17:35:29 +0900

    -

--- assets/portfolio/portfolio_Stove/js/common_js.js
+++ assets/portfolio/portfolio_Stove/js/common_js.js
@@ -3,13 +3,27 @@ $('a[href="#"]').on('click', function (e) {
 });
 
 
+$("body").on("click" , function (e) {$(e.target).parents("#games")[0] === undefined ? $("#games").find("li.on").removeClass('on') : onAdder(e.target);});
+setSlides("#section1 > div.slide_container");
 
 
-let onAdder = (e , container) => {
-  let selector = e.target;
+function onAdder(e , container) {
+  let selector = e;
   let parents = $(container)
   $(selector).closest('li').siblings('.on').removeClass('on')
   $(selector).closest('li').toggleClass('on');
 }
+function setSlides(selector) {
+  let $selector = $(selector);
+  let numSlide = $selector.find(".slides > li").length;
+  $selector.find('.slides > li').each((i) => {
+    $selector.find('.indicator').append('<li><a href="#"><span  class="hide_clipPath">' + (i + 1) + '번 슬라이드</span></a></li>\n');
+  });
+}
+function showSlide(n , selector){
+  $selector.find('.indicator li').removeClass('on');
+  $selector.find('.indicator li:eq(' + (n - 1) + ')').addClass('on');
+}
+
 
 //portfolio global
