be90eefc4b8abf1bd44997805646210fd77620b3
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2023-02-07 02:36:26 +0900

    -

--- assets/portfolio/portfolio_Stove/js/common_js.js
+++ assets/portfolio/portfolio_Stove/js/common_js.js
@@ -3,7 +3,10 @@ $('a[href="#"]').on('click', function (e) {
 });
 
 
-$("body").on("click" , function (e) {$(e.target).parents("#games")[0] === undefined ? $("#games").find("li.on").removeClass('on') : onAdder(e.target);});
+$("body").on("click" , function (e) {
+  $(e.target).parents("#games")[0] === undefined ? $("#games").find("li.on").removeClass('on') : onAdder(e.target);
+});
+
 setSlides("#section1 > div.slide_container");
 
 
@@ -15,14 +18,26 @@ function onAdder(e , container) {
 }
 
 function setSlides(selector) {
- 
-
-
+  let $selector = $(selector);
+  let numSlide = $selector.find(".slides > li").length;
+  let slideNow = 1;
+  let slideNext = slideNow + 1;
+  let slidePrev = slideNow - 1;
+  let slideIndex = $selector.find('.slides > li.on').index() + 1;
 
 
   $selector.find('.slides > li').each((i) => {
     $selector.find('.indicator').append('<li><a href="#"><span  class="hide_clipPath">' + (i + 1) + '번 슬라이드</span></a></li>\n');
   });
+  
+  $selector.find(".control > button").on("click" , function () {
+    $(this).attr('class') === 'right' ? showSlide(slideNext) : showSlide(slidePrev);
+  })
+
+  function showSlide(n) {
+    slideNow = n;
+    onAdder($selector.find('.slides > li:eq(' + n + ')'))
+  }
 }
 
 
