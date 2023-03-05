e136508d66173b4191f41683e1154851f24af74b
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2023-03-05 15:53:34 +0900

    -

--- assets/portfolio/portfolio_MC/js/common.js
+++ assets/portfolio/portfolio_MC/js/common.js
@@ -1,20 +1,14 @@
 // 나중에 selector 수정
 $("main ul li").on('click', function () {
-    $(this).toggleClass('on');
+    $(this).addClass('on');
     doOnClick();
-    // $("main ul li.on").css({"width" : 60+"%"});
-    // $("main ul li").not("li.on").css({"width": (40/3)+"%"});
-    // $("main ul li.on").css({"width" : 60+"%"});
-    // if ($("main ul li").hasClass('on')) {
-    //         $("header").css({"display":"none"});
-    //     } else {
-    //         $("main ul li").css({"width":25+"%"});
-    //         $("header").css({"display":"flex"});
-    //     }
 });
-$(".close").on('click', function () {
-    console.log(';ho')
-})
+$(".close").on('click', function (e) {
+    e.stopPropagation()
+    $("main ul li").removeClass('on');
+    doOnClick();
+});
+
 
 function doOnClick() {
     $("main ul li.on").css({"width" : 60+"%"});
