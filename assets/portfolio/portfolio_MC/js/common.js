0ae61dc294d57a1f8d35f16419fbcf20caff5993
cakwhdtj <cakwhdtj@gmail.com>
2023-03-16 18:39:15 +0900

    -

--- assets/portfolio/portfolio_MC/js/common.js
+++ assets/portfolio/portfolio_MC/js/common.js
@@ -1,11 +1,11 @@
 // 나중에 selector 수정
-$("main ul li").on('click', function () {
+$("main > ul > li").on('click', function () {
     $(this).addClass('on');
     doOnClick();
 });
 $(".close").on('click', function (e) {
     e.stopPropagation()
-    $("main ul li").removeClass('on');
+    $("main > ul > li").removeClass('on');
     doOnClick();
     $(".telecom").css({"transform" : "translate(-50%,-50%) scale(1)"})
 
@@ -14,13 +14,13 @@ $(".close").on('click', function (e) {
 
 
 function doOnClick() {
-    $("main ul li.on").css({"width" : 60+"%"});
-    $("main ul li").not("li.on").css({"width": (40/3)+"%"});
-    $("main ul li.on").css({"width" : 60+"%"});
-    if ($("main ul li").hasClass('on')) {
+    $("main > ul > li.on").css({"width" : 60+"%"});
+    $("main > ul > li").not("li.on").css({"width": (40/3)+"%"});
+    $("main > ul > li.on").css({"width" : 60+"%"});
+    if ($("main > ul > li").hasClass('on')) {
             $("header").css({"display":"none"});
         } else {
-            $("main ul li").css({"width":25+"%"});
+            $("main > ul > li").css({"width":25+"%"});
             $("header").css({"display":"flex"});
         };
     $(".telecom").css({"transform" : "translate(-50%,-50%) scale(.5)"})
