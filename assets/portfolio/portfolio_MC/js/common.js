f7b4372a75b61b108771d109f2553ba2f2591b2b
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2023-03-17 16:41:01 +0900

    -

--- assets/portfolio/portfolio_MC/js/common.js
+++ assets/portfolio/portfolio_MC/js/common.js
@@ -1,14 +1,12 @@
-// 나중에 selector 수정
 $("main > ul > li").on('click', function () {
     $(this).addClass('on');
     $("header").css({"display" : "none"});
-    // $(".telecom").css({"transform" : "translate(-50%,-50%) scale(.8)"})
-
+    var numOn = $(this).siblings(".on").length;
 });
 
 $(".close").on('click', function (e) {
     e.stopPropagation()
     $("main > ul > li").removeClass('on');
     $("header").removeAttr("style");
-    $(".telecom").removeAttr("style")
+    $(".telecom").removeAttr("style");
 });
