e2eb75cc09df90b937f9fca9a32b349ff06ec7af
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2023-03-17 15:19:15 +0900

    -

--- assets/portfolio/portfolio_MC/js/common.js
+++ assets/portfolio/portfolio_MC/js/common.js
@@ -1,29 +1,14 @@
 // 나중에 selector 수정
 $("main > ul > li").on('click', function () {
     $(this).addClass('on');
-    doOnClick();
+    $("header").css({"display" : "none"});
+    // $(".telecom").css({"transform" : "translate(-50%,-50%) scale(.8)"})
+
 });
+
 $(".close").on('click', function (e) {
     e.stopPropagation()
     $("main > ul > li").removeClass('on');
-    doOnClick();
-    $(".telecom").css({"transform" : "translate(-50%,-50%) scale(1)"})
-
+    $("header").removeAttr("style");
+    $(".telecom").removeAttr("style")
 });
-
-
-
-function doOnClick() {
-    $("main > ul > li.on").css({"width" : 60+"%"});
-    $("main > ul > li").not("li.on").css({"width": (40/3)+"%"});
-    $("main > ul > li.on").css({"width" : 60+"%"});
-    if ($("main > ul > li").hasClass('on')) {
-            $("header").css({"display":"none"});
-        } else {
-            $("main > ul > li").css({"width":25+"%"});
-            $("header").css({"display":"flex"});
-        };
-    $(".telecom").css({"transform" : "translate(-50%,-50%) scale(.5)"})
-
-    
-}
