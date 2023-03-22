0aeeb3871254d04475b4de5afb9be79835ec3abf
cakwhdtj <cakwhdtj@gmail.com>
2023-03-22 20:36:08 +0900

    -

--- assets/portfolio/portfolio_MC/js/common.js
+++ assets/portfolio/portfolio_MC/js/common.js
@@ -1,15 +1,31 @@
+let clickedLis = [];
+
 $("main > ul > li").on('click', function () {
-    $(this).addClass('on');
     $("header").css({"display" : "none"});
+    const $this = $(this);
+    
+    if (!clickedLis.includes($this[0])) {
+        clickedLis.push($this[0]);
+        $this.addClass('on');
+        
+        if (clickedLis.length > 2) {
+        const $firstLi = $(clickedLis.shift());
+        $firstLi.toggleClass('on');
+        }
+    } else {
+        clickedLis.splice(clickedLis.indexOf($this[0]), 1);
+        $this.toggleClass('on');
+    }
 });
 
 $(".close").on('click', function (e) {
-    e.stopPropagation()
-    $(this).parents("li.on").removeClass("on");
+    e.stopPropagation();
+    const $li = $(this).parents("li.on");
+    const liIndex = clickedLis.indexOf($li[0]);
+    if (liIndex !== -1) {
+      clickedLis.splice(liIndex, 1);
+    }
+    $li.removeClass("on");
     $("header").toggle(!$('li.on').length);
     $(".telecom").removeAttr("style");
-});
-
-
-// var footerHTML = $('footer').html();
-// $('.innerFooter').html(footerHTML);
+  });
