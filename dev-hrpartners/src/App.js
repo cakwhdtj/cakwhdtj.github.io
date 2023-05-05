a899a5cdab32da7cbde3ecf9c2901bbffe024453
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2023-05-05 17:44:20 +0900

    - hr

--- dev-hrpartners/src/App.js
+++ dev-hrpartners/src/App.js
@@ -5,13 +5,16 @@ const App = (props) => {
   const [showHeader, setShowHeader] = useState(true);
   const [showContent, setShowContent] = useState(true);
   const [showBg, setShowBg] = useState(true);
+  const [deltaY, setDeltaY] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const position = window.scrollY;
+      const newDeltaY = (position / window.innerHeight) * 100;
       setShowHeader(position < window.innerHeight);
       setShowContent(position < window.innerHeight);
       setShowBg(position < window.innerHeight); 
+      setDeltaY(newDeltaY);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -20,31 +23,39 @@ const App = (props) => {
 
   return (
     <div className="App">
+      <a href="#main" id="skipNav" onClick={(e) => {
+      e.preventDefault();
+      const mainOffsetTop = document.querySelector('main').offsetTop;
+      window.scrollTo({ top: mainOffsetTop, behavior: 'smooth' });
+    }}>본문으로</a>
       <div className={`landingEffect${showContent ? ' fade-in' : ''}`}>
         <div className={`bg-image${showBg ? ' fade-in' : ''}`}></div>
-        <div className="logoDiv">
+        <div className="logoDiv" style={{ left: `-${deltaY}%` }}>
           <h1 className={`logo${showHeader ? ' fade-in-left' : ''}`}>logo</h1>
         </div>
-        <div className={`textDiv${showContent ? ' fade-in-top' : ''}`}>
+        <div className={`textDiv${showContent ? ' fade-in-top' : ''}`} style={{ top: `-${deltaY}%` }}>
           <div className={`${showContent ? ' fade-in-top' : ''}`}>아무나 할 수 있는 곳이 아닌,</div>
           <div className={`${showContent ? ' fade-in-top' : ''}`}>아무도 할 수 없는 곳에 있는</div>
           <div className={`${showContent ? ' fade-in-top' : ''}`}>최고의 파트너</div>
           <div className={`${showContent ? ' fade-in-top' : ''}`}>HR PARTNERS</div>
         </div>
       </div>
-      <header>
-        <nav>
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-          </ul>
-        </nav>
-      </header>
-      <body>
-        <main>ddd</main>
-      </body>
+      <nav>
+        <ul style={{}}>
+          <li>인사말</li>
+          <li>사업자등록증</li>
+          <li>조직도</li>
+          <li>사업영역</li>
+          <li>실적안내</li>
+        </ul>
+      </nav>
+      <main>
+        <section id='sectoin1'>1</section>
+        <section id='section2'>2</section>
+        <section id='section3'>3</section>
+        <section id='sectoin4'>4</section>
+        <section id='sectoin5'>5</section>
+      </main>
     </div>
   );
 }
