0c0b78558d62fd50c13c7450d0335f8f2210d390
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2023-05-11 22:59:15 +0900

    hr

--- dev-hrpartners/src/App.js
+++ dev-hrpartners/src/App.js
@@ -46,28 +46,36 @@ const App = (props) => {
         </ul>
       </nav>
       <main>
-        <section id='sectoin1'>
+        <section id='section1'>
           <div>
-            <p>안녕하십니까 ㈜ 에이치알파트너스 대표이사 이해리 입니다.</p>
             <p>
+              <span className='fade-in-bottom'>안녕하십니까 ㈜ 에이치알파트너스 대표이사 이해리 입니다.</span>
+              <br></br><br></br>
+              <span className='fade-in-bottom'>
               ㈜ 에이치알파트너스는 클라이언트 고객들에게 최고의 성과 
               그리고 맞춤형 서비스를 제공하기 위해 노력하며 창조적인 변화, 지속적인 발전으로
               부동산 및 분양 업계를 선도하는 분양대행사로 자리매김 하고 있습니다.
-
+              </span>
+              <br></br><br></br>
+              <span className='fade-in-bottom'>
               저희 임직원은 모든 역량을 결집하여 최상의 컨디션과 시스템을 확립하고 ㈜ 에이치알파트너스를
               선택해주신 클라이언트의 사업에 있어 최선의 노력과 최고의 성과로 보답 드리고자 끝없는 도전,
               뜨거운 열정을 내세워 클라이언트의 선택이 완벽하게 옳았다는 것을 증명하겠습니다.
-
+              </span>
+              <br></br><br></br>
+              <span className='fade-in-bottom'>
               고객의 만족과 신뢰를 최상의 가치로 삼고 계속해서 진화하는 분양 사업이 트렌드에 
               발맞추어 분양대행 업계의 뉴 패러다임을 앞장서겠습니다.
+              </span>
+              <br></br><br></br>
+              <span className='fade-in-bottom'>대표이사 <i>이해리</i></span>
             </p>
-            <p></p>
           </div>
         </section>
         <section id='section2'> <div>2</div> </section>
         <section id='section3'> <div>3</div> </section>
-        <section id='sectoin4'> <div>4</div> </section>
-        <section id='sectoin5'> <div>5</div> </section>
+        <section id='section4'> <div>4</div> </section>
+        <section id='section5'> <div>5</div> </section>
       </main>
     </div>
   );
